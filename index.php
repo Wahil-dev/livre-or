5e684f9d78b247085935e6d168af12044d804d57
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-02-08 12:13:42 +0100

    créer la structure principale de projet

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="wahil chettouf">
+    <title>Acceuil</title>
+</head>
+<body>
+    <header id="header">
+        <nav>
+            <ul class="menu">
+                <li class="box-lien"><a href="#"></a></li>
+                <li class="box-lien"><a href="#"></a></li>
+                <li class="box-lien"><a href="#"></a></li>
+                <li class="box-lien"><a href="#"></a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
